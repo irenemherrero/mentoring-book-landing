f6ccb3796f9344ddc436b656c2be55ec9fbb87a4
Irene Mateo Herrero <irenemherrero@gmail.com>
2025-02-12 21:52:49 +0100

    feat: improve styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ export default {
       fontFamily: {
         barlow: ['Barlow', 'sans-serif'],
       },
+      colors: {
+        primary: "#E83425",
+        secondary: "#091A2F"
+      }
     },
   },
   plugins: [],
